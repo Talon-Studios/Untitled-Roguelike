d1ca0a3b78f2098385dfd2166d1576f360884e52
thcheetah777 <thcheetah777@gmail.com>
2023-04-26 08:05:48 +0200

    Add borders lets goo

--- Assets/_Scripts/UI/InteractableUI.cs
+++ Assets/_Scripts/UI/InteractableUI.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InteractableUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
+    public Image border;
+
+    void Start() {
+        border.gameObject.SetActive(false);
+    }
+
     public void OnPointerEnter(PointerEventData eventData) {
         MousePointer.Instance.SetMouseState(MouseState.Interacting);
+        if (border != null) border.gameObject.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData eventData) {
         MousePointer.Instance.SetMouseState(MouseState.Aiming);
+        if (border != null) border.gameObject.SetActive(false);
     }
 
 }
--- Assets/_Scripts/UI/UpgradeCard.cs
+++ Assets/_Scripts/UI/UpgradeCard.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class UpgradeCard : MonoBehaviour, IPointerClickHandler
 {
 
     public TMP_Text titleText;
     public TMP_Text descriptionText;
+    
     [HideInInspector] public UpgradeObject upgrade;
 
     public void OnPointerClick(PointerEventData eventData) {
